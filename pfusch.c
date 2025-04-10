31aa78d9b2cfa0e00c65ec51f9428747d3bb01ef
Karl <208985@fhwn.ac.at>
2025-04-10 11:23:21 +0200

    zweite aufgabe

--- pfusch.c
+++ pfusch.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <unistd.h> // für execlp
+//#include <.h> //? neuer header
+#include <sys/wait.h>
+#include <time.h>
+
+
 
 typedef struct command_line_arguments
 {
@@ -49,10 +55,63 @@ cli_args parse_command_line(int const argc, char *argv[])
     return args;
 }
 
+int child_labour()
+{
+//    printf("I am %d, child of %d\n", getpid(), getppid());
+
+    printf("[%d] doing some work for (%d)\n", getpid(), getppid());
+    srand(getpid());
+    sleep(2);
+    printf("[%d] jobs done\n", getpid());
+    printf("[%d] bringing coal to (%d)\n", getpid(), getppid());
+
+    // *(int *)(17) = 0; // führt zu absturz
+
+    return -1;
+}
+
 int main(int argc, char *argv[])
 {
+    /*
     cli_args const args = parse_command_line(argc, argv);
     printf("\ni: %d, s: %s, b: %d\n", args.i, args.s, args.b);
+    */
+
+
+    printf("[%d] sending child into the mines...\n", getpid());
+
+    pid_t forked = fork(); //pid_t für PID (int auch möglich, nur form sache)
+    if (forked == 0)
+    {
+        // execlp("ls", "ls", "-l", NULL);
+        return child_labour();
+    }
+
+    printf("my PID is %d, fork returned %d\n", getpid(), forked);
+
+    printf("[%d] enjoying some brandy...\n", getpid());
+    printf("[%d] where the fudge is my coal?\n", getpid());
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int wstatus = 0;
+        pid_t const waited = wait(&wstatus);
+
+        if(WIFEXITED(wstatus))
+        {
+            printf("[%d] child %d, exited normally with return code %d\n", getpid(), waited, WEXITSTATUS(wstatus));
+        }
+        else if (WIFSIGNALED(wstatus))
+        {
+            printf("[%d] child %d, terminated with signal %d\n", getpid(), waited, WTERMSIG(wstatus));
+        }
+        else
+        {
+            printf("[%d] child %d, terminated abnormaly %d\n", getpid(), waited, WTERMSIG(wstatus));
+        }
+    }
+
+    printf("all children have returned\n");
 
     return 0;
 }
